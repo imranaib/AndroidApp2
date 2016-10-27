88c643629ad9ffc7aad48b5eb4879e8862c812f4
imranaib <imransyed99@gmail.com>
2016-10-27 13:52:40 +0100

    Android App with Unit Test

---
+++ complete/src/test/java/org/hello/HelloActivityTest.java
@@ -0,0 +1,15 @@
+package org.hello;
+
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class HelloActivityTest {
+	
+	@Test
+	public void printTest() {
+		System.out.println("Hello Android");
+		assertEquals(" 0 = 0",0,0);
+	}
+}
